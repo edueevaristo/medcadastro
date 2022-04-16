eec5536546395fc82442607bd6511218849f62b7
Eduardo Evaristo <91930037+edueevaristo@users.noreply.github.com>
2022-04-15 21:13:08 -0300

    Update users_model.php

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -2,16 +2,16 @@
 
 class Users_model extends CI_Model {
 
-    public function store($users)
-    {
-        $this->db->insert("tb_users", $users);
-    }
-
     public function index()
     {
         return $this->db->get("tb_users")->result_array();
     }
 
+    public function store($user)
+    {
+        $this->db->insert("tb_users", $user);
+    }
+    
     //método para mostrar um usuário já adicionado para realizar a edição do mesmo
 	public function show($id)
 	{
@@ -19,5 +19,19 @@ class Users_model extends CI_Model {
 			"id" => $id
 		))->row_array();
 	}
+
+
+    public function update($id, $user)
+		{
+			$this->db->where("id", $id);
+			return $this->db->update("tb_users", $user);
+		}
+
+
+    public function deletar($id)
+    {
+        $this->db->where("id", $id);
+        return $this->db->delete("tb_users");
+    }
 
 }
